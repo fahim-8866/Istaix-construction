6b490c3f7f00457d1a307ba59b355e6dd040e01a
Fahim Muntasir <fmuntasir39@gmail.com>
2024-09-15 22:05:19 +0600

    javascript construc

---
+++ main.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     $(".client_imgs").on("click", "a",function(){
+//         var tabId= $(this).attr("id");
+//         $(this).addClass("active").siblings().removeClass("active");
+//         $("#" + tabId + "-Comment-card").addClass("active").siblings().removeClass("active");
+//     });
+// });
+
+$('.client_imgs a').click(function(event){
+    index= $(this).index();
+
+    $('.client_imgs a').removeClass('active');
+    $(this).addClass('active');
+    $('.card_comment').hide();
+    $('.card_comment').eq(index).show();
+});
+
+
+$(document).ready(function($){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+  });
